70d3dd2fb26010ea3bc7575ce5aaaecfd3131b1b
Evgeny Pankov <volvovnl123@gmail.com>
2023-08-25 09:34:20 +0300

    HW #40 TCP client calculate/ getResponse- calculate

--- src/telran/net/calculator/TcpServerCalculator.java
+++ src/telran/net/calculator/TcpServerCalculator.java
@@ -1,7 +1,9 @@
 package telran.net.calculator;
 
 import java.net.*;
+import java.util.function.BinaryOperator;
 import java.io.*;
+import telran.view.*;
 
 public class TcpServerCalculator {
 	static final int PORT = 4000;
@@ -19,25 +21,48 @@ public class TcpServerCalculator {
 	private static void clientRun(Socket socket) {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintStream writer = new PrintStream(socket.getOutputStream())) {
-			while (true) {
-				String line = reader.readLine();
-				if (line == null) {
-					System.out.println("Client close normally connection");
-					break;
-				}
+			String line;
+			while ((line = reader.readLine()) != null) {
 				String response = getResponse(line);
 				writer.println(response);
 			}
+			String response = getResponse(line);
+			writer.println(response);
 		} catch (Exception e) {
 			System.out.println("Clien close abnormally connection");
 		}
-		;
-
 	}
 
 	private static String getResponse(String line) {
-		// TODO Auto-generated method stub
-		return null;
+		String response = "Wrong request structure";
+		String[] tokens = line.split("#");
+		if (tokens.length == 3) {
+			String requestType = tokens[0];
+			double firstOperand = Double.parseDouble(tokens[1]);
+			double secondOperand = Double.parseDouble(tokens[2]);
+			switch (requestType) {
+			case "add":
+				response = String.valueOf(firstOperand + secondOperand);
+				break;
+			case "subtract":
+				response = String.valueOf(firstOperand - secondOperand);
+				break;
+			case "multiply":
+				response = String.valueOf(firstOperand * secondOperand);
+				break;
+			case "divide":
+				if (secondOperand != 0) {
+					response = String.valueOf(firstOperand / secondOperand);
+				} else {
+					response = "Division by zero is not allowed";
+				}
+				break;
+			default:
+				response = "wrong request type";
+				break;
+			}
+		}
+		return response;
 	}
 
 }

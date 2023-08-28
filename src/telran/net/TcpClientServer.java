57079af665300a3b7f2ba4fb5573f72ca2490947
Evgeny Pankov <volvovnl123@gmail.com>
2023-08-28 15:57:56 +0300

    CW #41 TCP OOD / OOP

---
+++ src/telran/net/TcpClientServer.java
@@ -0,0 +1,34 @@
+package telran.net;
+
+import java.net.*;
+import java.io.*;
+
+public class TcpClientServer implements Runnable {
+	Socket socket;
+	ObjectInputStream input;
+	ObjectOutputStream output;
+	ApplProtocol protocol;
+
+	public TcpClientServer(Socket socket, ApplProtocol protocol) throws IOException {
+		this.socket = socket;
+		input = new ObjectInputStream(socket.getInputStream());
+		output = new ObjectOutputStream(socket.getOutputStream());
+		this.protocol = protocol;
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				Request request = (Request) input.readObject();
+				Response response = protocol.getResponse(request);
+				output.writeObject(response);
+			}
+		} catch (EOFException e) {
+			System.out.println("Client closed normally connection");
+		} catch (Exception e) {
+			System.out.println("Client closed abnormally connection" + e.getMessage());
+		}
+	}
+
+}

c61624308037eb54336a2654d5cf449a29566c19
Evgeny Pankov <volvovnl123@gmail.com>
2023-08-30 17:08:44 +0300

    HW #41 Solution calculator with TCP OOD

--- src/telran/net/CalculatorProtocol.java
+++ src/telran/net/CalculatorProtocol.java
@@ -4,27 +4,23 @@ public class CalculatorProtocol implements ApplProtocol {
 
 	@Override
 	public Response getResponse(Request request) {
-		System.out.println(request);
-		String responseCalc = getResponseCalculate((Request) request.requestData());
-		Response response = new Response(ResponseCode.OK, responseCalc);
-		return (Response) response;
+		Response response = null;
+		try {
+			double[] operands = (double[]) request.requestData();
+			String requestType = request.requestType();
+			double res = switch (requestType) {
+			case "add" -> operands[0] + operands[1];
+			case "multiply" -> operands[0] * operands[1];
+			case "divide" -> operands[0] / operands[1];
+			case "minus" -> operands[0] - operands[1];
+			default -> Double.NaN;
+			};
+			response = Double.isNaN(res) ? new Response(ResponseCode.WRONG_TYPE, requestType + " is wrong request type")
+					: new Response(ResponseCode.OK, res);
+			return response;
+		} catch (Exception e) {
+			return new Response(ResponseCode.WRONG_DATA, e.getMessage());
+		}
 	}
 
-	private String getResponseCalculate(Request request) {
-		String responseData = "Wrong request structure, usage: <operation type>#<operand 1>#<operand 2>";
-		try {
-			double[] operand = (double[]) request.requestData();
-			responseData = switch (request.requestType()) {
-			case "add" -> Double.toString(operand[0] + operand[1]);
-			case "minus" -> Double.toString(operand[0] - operand[1]);
-			case "multiply" -> Double.toString(operand[0] * operand[1]);
-			case "divide" ->
-				operand[1] != 0 ? String.valueOf(operand[0] / operand[1]) : "Division by zero is not allowed";
-			default -> "wrong request type";
-			};
-		} catch (Exception e) {
-			responseData = e.getMessage();
-		}
-		return responseData;
-	}
 }
--- src/telran/net/CalculatorTcpClientAppl.java
+++ src/telran/net/CalculatorTcpClientAppl.java
@@ -9,25 +9,19 @@ public class CalculatorTcpClientAppl {
 	static final int PORT = 5000;
 
 	public static void main(String[] args) throws Exception {
-		try (TcpHandler tcpHandler = new TcpHandler("localhost", PORT)) {
+		try (TcpHandler tcpHandler = new TcpHandler(HOST, PORT);) {
 			InputOutput io = new ConsoleInputOutput();
 			Menu menu = new Menu("Calculator Application", Item.of("send request", io1 -> {
 				HashSet<String> requests = new HashSet<>(Arrays.asList("add", "minus", "multiply", "divide"));
 				String requestType = io1.readString("Enter operation type " + requests, "Wrong operation", requests);
-				double operand1 = io1.readDouble("Enter first number", "Wrong number");
-				double operand2 = io1.readDouble("Enter second number", "Wrong number");
+				double op1 = io1.readDouble("Enter first number", "Wrong number");
+				double op2 = io1.readDouble("Enter second number", "Wrong number");
+				double res = tcpHandler.send(requestType, new double[] { op1, op2 });
+				io1.writeLine(res);
 
-				double[] requestData = { operand1, operand2 };
-
-				Request request = new Request(requestType, requestData);
-
-				String response = tcpHandler.send(requestType, request);
-
-				io1.writeLine(response);
 			}), Item.ofExit());
 			menu.perform(io);
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
+
 	}
 }

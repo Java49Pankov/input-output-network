718d78ade3d869977e0469e67a5d3275019e96c4
Evgeny Pankov <volvovnl123@gmail.com>
2023-08-31 15:15:41 +0300

    CW #42 Company client/server application

---
+++ src/telran/employees/CompanyServerAppl.java
@@ -0,0 +1,23 @@
+package telran.employees;
+
+import java.io.IOException;
+
+import telran.employees.service.*;
+import telran.net.TcpServer;
+
+public class CompanyServerAppl {
+
+	private static String fileName;
+	private static final String DEFAULT_FILE_NAME = "employees.data";
+	private static final int PORT = 5000;
+
+	public static void main(String[] args) throws IOException {
+		fileName = args.length > 0 ? args[0] : DEFAULT_FILE_NAME;
+		Company company = new CompanyImpl();
+		company.restore(fileName);
+
+		TcpServer tcpServer = new TcpServer(PORT, new CompanyProtocol(company));
+		tcpServer.run();
+	}
+
+}
